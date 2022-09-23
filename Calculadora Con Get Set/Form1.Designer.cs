4c46b96432de4a227cc4879480bb70ea0b6e22fd
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-23 09:17:14 -0600

    Agregar archivos de proyecto.

---
+++ Calculadora Con Get Set/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Calculadora_Con_Get_Set
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtv1 = new System.Windows.Forms.TextBox();
+            this.txtv2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.btnResta = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor 1";
+            // 
+            // txtv1
+            // 
+            this.txtv1.Location = new System.Drawing.Point(128, 43);
+            this.txtv1.Name = "txtv1";
+            this.txtv1.Size = new System.Drawing.Size(100, 20);
+            this.txtv1.TabIndex = 1;
+            // 
+            // txtv2
+            // 
+            this.txtv2.Location = new System.Drawing.Point(128, 78);
+            this.txtv2.Name = "txtv2";
+            this.txtv2.Size = new System.Drawing.Size(100, 20);
+            this.txtv2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Valor 2";
+            // 
+            // txtres
+            // 
+            this.txtres.Location = new System.Drawing.Point(128, 123);
+            this.txtres.Name = "txtres";
+            this.txtres.Size = new System.Drawing.Size(100, 20);
+            this.txtres.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado";
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(256, 64);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(30, 31);
+            this.btnSuma.TabIndex = 6;
+            this.btnSuma.Text = "+";
+            this.btnSuma.UseVisualStyleBackColor = true;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // btnResta
+            // 
+            this.btnResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnResta.Location = new System.Drawing.Point(292, 64);
+            this.btnResta.Name = "btnResta";
+            this.btnResta.Size = new System.Drawing.Size(30, 31);
+            this.btnResta.TabIndex = 7;
+            this.btnResta.Text = "-";
+            this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
+            // 
+            // btnMult
+            // 
+            this.btnMult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMult.Location = new System.Drawing.Point(256, 101);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(30, 31);
+            this.btnMult.TabIndex = 8;
+            this.btnMult.Text = "X";
+            this.btnMult.UseVisualStyleBackColor = true;
+            this.btnMult.Click += new System.EventHandler(this.btnMult_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(292, 101);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(30, 31);
+            this.btnDiv.TabIndex = 9;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 196);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnResta);
+            this.Controls.Add(this.btnSuma);
+            this.Controls.Add(this.txtres);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtv2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtv1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtv1;
+        private System.Windows.Forms.TextBox txtv2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSuma;
+        private System.Windows.Forms.Button btnResta;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ Calculadora Con Get Set/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_Con_Get_Set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            clsuma sum = new clsuma();
+            sum.V1 = int.Parse(txtv1.Text);
+            sum.V2 = int.Parse(txtv2.Text);
+
+            txtres.Text = sum.sumar().ToString();
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            clresta res = new clresta();
+            res.V1 = int.Parse(txtv1.Text);
+            res.V2 = int.Parse(txtv2.Text);
+
+            txtres.Text = res.restar().ToString();
+        }
+
+        private void btnMult_Click(object sender, EventArgs e)
+        {
+            clmultiplicacion mult = new clmultiplicacion();
+            mult.V1 = int.Parse(txtv1.Text);
+            mult.V2 = int.Parse(txtv2.Text);
+
+            txtres.Text = mult.multiplicar().ToString();
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            cldivision div = new cldivision(); 
+            div.V1 = double.Parse(txtv1.Text);
+            div.V2 = double.Parse(txtv2.Text);
+
+            txtres.Text = div.dividir().ToString();
+
+        }
+    }
+}
